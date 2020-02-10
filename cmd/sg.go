31dfdd5f380e34d427f1fd73b40c740e9e554ec0
dechiad1 <dechiad1@tcnj.edu>
2020-02-09 20:29:39 -0500

    add port range for add and remove

--- cmd/sg.go
+++ cmd/sg.go
@@ -18,6 +18,7 @@ func init() {
 	sgListGroups.PersistentFlags().StringVarP(&GroupNameFilter, "filter", "f", "", "filter sg based on if the value of this flag is in the name of the group")
 	sgAddLocalGroup.PersistentFlags().StringVarP(&GroupIDInput, "group", "g", "", "group id of sg to add rule to")
 	sgAddLocalGroup.PersistentFlags().StringVarP(&cidrRange, "cidrRange", "r", "", "cidr range of ip to add rule for")
+	sgAddLocalGroup.PersistentFlags().StringVarP(&portRange, "portRange", "p", "22", "port range to create TCP allow for. Must be in the format of ##-##")
 	sgCmd.AddCommand(sgRemoveLocalGroup)
 	sgCmd.AddCommand(sgListGroups)
 	sgCmd.AddCommand(sgAddLocalGroup)
@@ -34,6 +35,8 @@ var (
 	cidrRange string
 	//GawsRuleName is used as a description for the rule
 	GawsRuleName string = "IP by gaws - current location"
+	//portRange is an optional flag to specify what ports to open on a created SG rule. defaults to just 22
+	portRange string = "22"
 )
 
 //commands
@@ -53,7 +56,7 @@ var sgCmd = &cobra.Command{
 var sgRemoveLocalGroup = &cobra.Command{
 	Use:   "rm",
 	Short: "wipe the gaws rules that have been added",
-	Long:  "remove all the gaws rules that have been added via and are identified with the GawsRuleName",
+	Long:  "remove all the gaws rules that have been added via gaws and are identified with the GawsRuleName",
 	Run: func(cmd *cobra.Command, args []string) {
 		input := &ec2.DescribeSecurityGroupsInput{
 			GroupIds: aws.StringSlice(args),
@@ -68,13 +71,20 @@ var sgRemoveLocalGroup = &cobra.Command{
 			//fmt.Println("name: ", *group.GroupName)
 			for _, permission := range group.IpPermissions {
 				for _, ip := range permission.IpRanges {
+					//check if the field exists in the struct - if not, pointer will be nil
+					if ip.Description == nil {
+						continue
+					}
 					if *ip.Description == GawsRuleName {
+						fromPort := permission.FromPort
+						toPort := permission.ToPort 
+						
 						input := &ec2.RevokeSecurityGroupIngressInput{
 							GroupId:    group.GroupId,
 							IpProtocol: permission.IpProtocol,
 							CidrIp:     ip.CidrIp,
-							FromPort:   aws.Int64(22),
-							ToPort:     aws.Int64(22),
+							FromPort:   fromPort, //aws.Int64(22),
+							ToPort:     toPort, //aws.Int64(22),
 						}
 						req, resp := svc.RevokeSecurityGroupIngressRequest(input)
 						err := req.Send()
@@ -104,6 +114,7 @@ func addLocalGroup(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
+	// obtain the IP address to add the rule for
 	ip := util.GetPublicIp()
 	var cidr string
 	if cidrRange != "" {
@@ -112,11 +123,14 @@ func addLocalGroup(cmd *cobra.Command, args []string) {
 		cidr = ip + "/32"
 	}
 
+	// obtain the ports to add the rule for
+	fromPort, toPort := getPortRange(portRange)
+
 	input := &ec2.AuthorizeSecurityGroupIngressInput{
 		GroupId: aws.String(GroupIDInput),
 		IpPermissions: []*ec2.IpPermission{
 			{
-				FromPort:   aws.Int64(22),
+				FromPort:   aws.Int64(fromPort),
 				IpProtocol: aws.String("tcp"),
 				IpRanges: []*ec2.IpRange{
 					{
@@ -124,7 +138,7 @@ func addLocalGroup(cmd *cobra.Command, args []string) {
 						Description: aws.String("IP by gaws - current location"),
 					},
 				},
-				ToPort: aws.Int64(22),
+				ToPort: aws.Int64(toPort),
 			},
 		},
 	}
@@ -145,6 +159,24 @@ func calculateCidrRange(ip string, cidr string) (ipRange string) {
 	parts[3] = "0"
 	ipRange = strings.Join(parts, ".") + "/24"
 	return ipRange
+}
+
+func getPortRange(portRange string) (int64, int64) {
+	if portRange == "22" {
+		return 22, 22
+	}
+	r := strings.Split(portRange,"-")
+	if len(r) != 2 {
+		fmt.Println("portRange must be in the format of ##-##. ie 8000-8080")
+		panic("invalid port range")
+	}
+	fp, err := strconv.ParseInt(r[0], 10, 64) //string to parse, numerical base, size int (int64)
+	tp, err := strconv.ParseInt(r[1], 10, 64)
+	if err != nil {
+		fmt.Printf("%s, %s are not ints\n", r[0], r[1])
+		panic(err)
+	}
+	return fp, tp
 }
 
 type gEC2 struct {
